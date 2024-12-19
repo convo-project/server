512edd2f8d8765c3a7b573f1c8baf1b0371bb99a
cbjoon <qudwns017@naver.com>
2024-12-19 19:08:17 +0900

    Init: 프로젝트 초기 세팅

---
+++ src/main/java/com/bj/convo/ConvoApplication.java
@@ -0,0 +1,13 @@
+package com.bj.convo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConvoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConvoApplication.class, args);
+    }
+
+}
