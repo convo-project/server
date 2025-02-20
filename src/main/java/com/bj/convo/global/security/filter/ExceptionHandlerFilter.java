fa9a0758bac8ed65c5aa3357ab5a10bebc0dc8d2
qudwns017 <qudwns017@naver.com>
2025-02-21 03:51:50 +0900

    Fix: ExceptionHandlerFilter 오류 수정

--- src/main/java/com/bj/convo/global/security/filter/ExceptionHandlerFilter.java
+++ src/main/java/com/bj/convo/global/security/filter/ExceptionHandlerFilter.java
@@ -1,20 +1,17 @@
 package com.bj.convo.global.security.filter;
 
+import com.bj.convo.global.security.exception.SecurityErrorCode;
 import com.bj.convo.global.util.response.ErrorResponse;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import io.jsonwebtoken.ExpiredJwtException;
-import io.jsonwebtoken.MalformedJwtException;
-import io.jsonwebtoken.security.SignatureException;
+import io.jsonwebtoken.JwtException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import javax.security.sasl.AuthenticationException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 @Slf4j
@@ -27,10 +24,21 @@ public class ExceptionHandlerFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
         try {
-            filterChain.doFilter(request, response);  // 다음 필터로 요청 전달
-        } catch (Exception e) {  // 최상위 예외를 추가로 처리
+            filterChain.doFilter(request, response);
+        } catch (JwtException e) {
+            log.error("토큰 만료: {}", e.getMessage(), e);
+            if (e.getMessage().equals(SecurityErrorCode.EXPIRED_TOKEN.getMessage())) {
+                setErrorResponse(SecurityErrorCode.EXPIRED_TOKEN.getHttpStatus(),
+                        SecurityErrorCode.EXPIRED_TOKEN.getMessage(), request, response);
+            } else {
+                log.error("토큰 오류: {}", e.getMessage(), e);
+                setErrorResponse(SecurityErrorCode.UNKNOWN_TOKEN_ERROR.getHttpStatus(),
+                        SecurityErrorCode.UNKNOWN_TOKEN_ERROR.getMessage(), request, response);
+            }
+        } catch (Exception e) {
             log.error("알 수 없는 예외 발생: {}", e.getMessage(), e);
-            setErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR, "Internal Server Error", request, response);
+            setErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR, "서버가 사용자의 요청을 처리하는 과정에서 내부 오류가 발생했습니다.", request,
+                    response);
         }
     }
 
